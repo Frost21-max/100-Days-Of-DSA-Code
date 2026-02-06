f4260ffaf2c7a6e002925c74d9708243342c6a5a
Frost21-max <bishtpiyush363@gmail.com>
2026-02-06 19:53:06 +0530

    Changed by Piyush Bisht

--- 590027949-piyushbisht-001-Q1.c
+++ 590027949-piyushbisht-001-Q1.c
@@ -1,28 +1,44 @@
 #include <stdio.h>
+
 int main() {
-    int n, pos, x,size;
-    printf("Enter the size of the array: ");
+    int n, pos, x, size;
+
+    printf("Enter the size of the array:\n");
     scanf("%d", &size);
+
     int arr[size];
 
+    printf("Enter number of elements:\n");
     scanf("%d", &n);
+
+    if (n >= size) {
+        printf("Array is full\n");
+        return 0;
+    }
+
+    printf("Enter elements:\n");
     for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
 
+    printf("Enter position and element:\n");
     scanf("%d %d", &pos, &x);
+
+    if (pos < 1 || pos > n + 1) {
+        printf("Invalid position\n");
+        return 0;
+    }
 
     for (int i = n; i >= pos; i--) {
         arr[i] = arr[i - 1];
     }
 
     arr[pos - 1] = x;
+    n++;
 
-    for (int i = 0; i <= n; i++) {
-        printf("%d", arr[i]);
-        if (i < n) printf(" ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
     }
-    printf("\n");
 
     return 0;
 }
